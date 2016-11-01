17b15dcc13d48e4899e41e4018060cfb132c406a
B. Travis Howe <b.travis.howe@gmail.com>
2016-11-01 10:16:14 -0600

    db scraper

--- allydvm_tables_download.py
+++ allydvm_tables_download.py
@@ -4,13 +4,13 @@ import os
 import csv
 
 
-filename = open('clients.csv', 'w')
+filename = open('../clients.csv', 'w')
 c = csv.writer(filename)
 
 config = {
   'user': os.getenv('ALLYDVM_USER_NAME'),
   'password': os.getenv('ALLYDVM_USER_PASSWORD'),
-  'host': 'db.allydvm.com',
+  'host': os.getenv('ALLYDVM_HOST'),
   'database': 'pulsar',
   'port': '3306'
 }
@@ -18,7 +18,7 @@ config = {
 conn = mysql.connector.connect(**config)
 crsr = conn.cursor()
 # query = ("SELECT * FROM invoice_items ii JOIN products p ON ii.product_id=p.product_id LIMIT 10")
-query = ("(SELECT 'source_id', 'invoice_item_id', 'practice_id', 'transaction_id', 'patient_id', 'product_id', 'last_synced_at', 'quantity', 'price', 'description', 'address', 'city', 'state', 'postal_code') UNION (SELECT ii.source_id, ii.invoice_item_id, ii.practice_id, ii.transaction_id, ii.patient_id, ii.product_id, ii.last_synced_at, ii.quantity, ii.price, p.description, pr.address, pr.city, pr.state, pr.postal_code FROM invoice_items ii JOIN products p ON ii.product_id=p.product_id JOIN practices pr ON ii.practice_id=pr.id LIMIT 10)")
+query = ("SELECT ii.source_id, ii.invoice_item_id, ii.practice_id, ii.transaction_id, ii.patient_id, ii.product_id, ii.last_synced_at, ii.quantity, ii.price, p.description, pr.address, pr.city, pr.state, pr.postal_code FROM invoice_items ii JOIN products p ON ii.product_id=p.product_id JOIN practices pr ON ii.practice_id=pr.id WHERE p.description rlike 'heartgard|heartguard|hartgard|hartguard' 'Sentinel|Sentinal' 'Proheart|prohart' 'Trifexis' 'trihart|triheart' 'iverhart|iverheart' 'interceptor|intercepter' 'revolution|revoluton' 'advantage Multi|advantagemulti' LIMIT 50")
 
 crsr.execute(query)
 
@@ -40,4 +40,4 @@ conn.close()
 
 
 # maybe I also need the tax? is this why jason uses the transaction database?
-# (SELECT 'source_id', 'invoice_item_id', 'practice_id', 'transaction_id', 'patient_id', 'product_id', 'last_synced_at', 'quantity', 'price', 'description', 'address', 'city', 'state', 'postal_code') UNION (SELECT ii.source_id, ii.invoice_item_id, ii.practice_id, ii.transaction_id, ii.patient_id, ii.product_id, ii.last_synced_at, ii.quantity, ii.price, p.description, pr.address, pr.city, pr.state, pr.postal_code FROM invoice_items ii JOIN products p ON ii.product_id=p.product_id JOIN practices pr ON ii.practice_id=pr.id LIMIT 10)
+# SELECT ii.source_id, ii.invoice_item_id, ii.practice_id, ii.transaction_id, ii.patient_id, ii.product_id, ii.last_synced_at, ii.quantity, ii.price, p.description, pr.address, pr.city, pr.state, pr.postal_code FROM invoice_items ii JOIN products p ON ii.product_id=p.product_id JOIN practices pr ON ii.practice_id=pr.id WHERE p.description rlike 'heartgard|heartguard|hartgard|hartguard' 'Sentinel|Sentinal' 'Proheart|prohart' 'Trifexis' 'trihart|triheart' 'iverhart|iverheart' 'interceptor|intercepter' 'revolution|revoluton' 'advantage Multi|advantagemulti' LIMIT 50;
