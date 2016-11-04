c464f60a04b954ea1e2973c8a4426ea2e6307780
B. Travis Howe <b.travis.howe@gmail.com>
2016-11-03 23:44:54 -0600

    stuff

--- variable_construct.py
+++ variable_construct.py
@@ -1,5 +1,8 @@
+from standard_of_living import mean_income
+from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
+import requests
 
 def sample_construct():
     df = pd.read_pickle('../data/invoice_pickle')
@@ -20,23 +23,36 @@ def patron_num():
     df_merged.to_pickle('../data/merged_ss')
 
 def patron_zip():
+    df = pd.read_csv('../data/14zpallagi.csv')
+    df_sum = df.groupby('zipcode')[['N02650', 'A02650']].sum()
+    df_sum.drop(df_sum.index[0], inplace=True)
+    df_sum['mean_income'] = df_sum['A02650']/df_sum['N02650']
+    df_sum.drop(['N02650', 'A02650'], 1, inplace=True)
+    df_sum['zip'] = df_sum.index.astype('str')
+    df_sum['zip'] = df_sum.zip.apply(lambda x: x.zfill(5))
+    df_sum.set_index('zip', inplace=True)
+
     df = pd.read_pickle('../data/merged_ss')
 
     practices = df.practice_id.unique()
+    df['weighted_income'] = 0
     for prac in practices:
         df_masked = df[df.practice_id==prac]
         N_practices = len(df_masked)
         zips = df_masked.client_postal_code.unique()
 
         weighted_income = 0
+
+        print(zips)
         for zip_code in zips:
+
+            zip_code = zip_code[:5]
             N_zip_code = len(df_masked[df_masked.client_postal_code==zip_code])
-            weighted_income += (N_zip_code/N_practices)*mean_income(zip_code)
-
-def mean_income(zip_code):
-    
+            weighted_income += (N_zip_code/N_practices) * float(df_sum.loc[zip_code].values)
 
 
+        df.ix[df.practice_id == prac, 'weighted_income'] = weighted_income
+    print(df.head())
 
 
 
@@ -45,3 +61,4 @@ if __name__=="__main__":
     # sample_construct()
     # patron_num()
     patron_zip()
+    # mean_income('60626')
