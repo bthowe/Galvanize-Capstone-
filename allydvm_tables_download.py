e1ace1745f9af0f24d2693e3634ea4ea6721849f
B. Travis Howe <b.travis.howe@gmail.com>
2016-11-04 11:34:26 -0600

    vastreamlining

--- allydvm_tables_download.py
+++ allydvm_tables_download.py
@@ -70,7 +70,7 @@ def invoice():
     print("Merged invoice scraper:")
     filename = open('../data/invoice_data.csv', 'w')
     c = csv.writer(filename)
-    c.writerow(('source_id', 'invoice_item_id', 'practice_id', 'transaction_id', 'patient_id', 'product_id', 'last_synced_at', 'quantity', 'price', 'description', 'practice_address', 'practice_city', 'practice_state', 'practice_postal_code', 'client_address', 'client_city', 'client_state', 'client_postal_code'))
+    c.writerow(('source_id', 'invoice_item_id', 'practice_id', 'transaction_id', 'patient_id', 'product_id', 'quantity', 'price', 'description', 'practice_enter_date', 'practice_address', 'practice_city', 'practice_state', 'practice_postal_code', 'transaction_client', 'transaction_date', 'transaction_type', 'transaction_amount', 'client_address', 'client_city', 'client_state', 'client_postal_code'))
 
     config = {
       'user': os.getenv('ALLYDVM_USER_NAME'),
@@ -83,7 +83,7 @@ def invoice():
     conn = mysql.connector.connect(**config)
     crsr = conn.cursor()
     print("Performing query now...\n")
-    query = ("SELECT ii.source_id, ii.invoice_item_id, ii.practice_id, ii.transaction_id, ii.patient_id, ii.product_id, ii.last_synced_at, ii.quantity, ii.price, p.description, pr.address, pr.city, pr.state, pr.postal_code, c.address, c.city, c.state, c.postal_code FROM invoice_items ii JOIN products p ON ii.product_id=p.product_id AND ii.source_id=p.source_id JOIN practices pr ON ii.practice_id=pr.id AND ii.source_id=pr.source_id JOIN transactions t ON ii.transaction_id=t.transaction_id AND ii.source_id=t.source_id JOIN clients c ON t.client_id=c.client_id AND t.source_id=c.source_id WHERE p.description rlike 'heartgard|heartguard|hartgard|hartguard|Sentinel|Sentinal|Proheart|prohart|Trifexis|trihart|triheart|iverhart|iverheart|interceptor|intercepter|revolution|revoluton|advantage Multi|advantagemulti'")
+    query = ("SELECT ii.source_id, ii.invoice_item_id, ii.practice_id, ii.transaction_id, ii.patient_id, ii.product_id, ii.quantity, ii.price, p.description, pr.first_synced_at, pr.address, pr.city, pr.state, pr.postal_code, t.client_id, t.posted_at, t.type, t.amount, c.address, c.city, c.state, c.postal_code FROM invoice_items ii JOIN products p ON ii.product_id=p.product_id AND ii.source_id=p.source_id JOIN practices pr ON ii.practice_id=pr.id AND ii.source_id=pr.source_id JOIN transactions t ON ii.transaction_id=t.transaction_id AND ii.source_id=t.source_id JOIN clients c ON t.client_id=c.client_id AND t.source_id=c.source_id WHERE p.description rlike 'heartgard|heartguard|hartgard|hartguard|Sentinel|Sentinal|Proheart|prohart|Trifexis|trihart|triheart|iverhart|iverheart|interceptor|intercepter|revolution|revoluton|advantage Multi|advantagemulti'")
     crsr.execute(query)
 
     print("Fetching rows now...\n")
@@ -96,11 +96,11 @@ def invoice():
     filename.close()
     conn.close()
 
-def practice_tr():
+def practice_total_revenue(practice_id):
     print("Practice total revenue scraper:")
-    filename = open('../data/practice_tr.csv', 'w')
-    c = csv.writer(filename)
-    c.writerow(('source_id', 'practice_id', 'total_revenue'))
+    # filename = open('../data/practice_tr.csv', 'w')
+    # c = csv.writer(filename)
+    # c.writerow(('source_id', 'practice_id', 'total_revenue'))
 
     config = {
       'user': os.getenv('ALLYDVM_USER_NAME'),
@@ -112,19 +112,50 @@ def practice_tr():
 
     conn = mysql.connector.connect(**config)
     crsr = conn.cursor()
-    print("Performing query now...\n")
-    query = ("SELECT source_id, practice_id, SUM(price) FROM invoice_items GROUP BY source_id, practice_id")
+    print("Performing query for 'practice_id' {0}...\n".format(practice_id))
+    query = ("SELECT SUM(amount) FROM transactions WHERE type=1 AND posted_at>'2015-11-01' AND practice_id={0}".format(practice_id))
     crsr.execute(query)
 
-    print("Fetching rows now...\n")
+    print("Fetching row...\n")
     row = crsr.fetchone()
-    while row is not None:
-        c.writerow(row)
-        row = crsr.fetchone()
+    return row
+    # while row is not None:
+    #     c.writerow(row)
+    #     row = crsr.fetchone()
 
     crsr.close()
-    filename.close()
+    # filename.close()
     conn.close()
+
+# def practice_tr():
+#     print("Practice total revenue scraper:")
+#     filename = open('../data/practice_tr.csv', 'w')
+#     c = csv.writer(filename)
+#     c.writerow(('source_id', 'practice_id', 'total_revenue'))
+#
+#     config = {
+#       'user': os.getenv('ALLYDVM_USER_NAME'),
+#       'password': os.getenv('ALLYDVM_USER_PASSWORD'),
+#       'host': os.getenv('ALLYDVM_HOST'),
+#       'database': 'pulsar',
+#       'port': '3306'
+#     }
+#
+#     conn = mysql.connector.connect(**config)
+#     crsr = conn.cursor()
+#     print("Performing query now...\n")
+#     query = ("SELECT source_id, practice_id, SUM(price) FROM invoice_items GROUP BY source_id, practice_id")
+#     crsr.execute(query)
+#
+#     print("Fetching rows now...\n")
+#     row = crsr.fetchone()
+#     while row is not None:
+#         c.writerow(row)
+#         row = crsr.fetchone()
+#
+#     crsr.close()
+#     filename.close()
+#     conn.close()
 
 def load_data_client():
     df = pd.read_csv('../data/clients.csv')
@@ -138,11 +169,6 @@ def load_data_employees():
     print(df.info())
     print(df.head())
 
-def load_data_invoice():
-    df = pd.read_csv('../data/invoice_data.csv')
-    df.to_pickle('../data/invoice_pickle')
-    print(df.info())
-    print(df.head())
 
 if __name__=="__main__":
     # clients()
--- variable_construct.py
+++ variable_construct.py
@@ -1,28 +1,57 @@
-from standard_of_living import mean_income
-from bs4 import BeautifulSoup
+from allydvm_tables_download import practice_total_revenue
 import pandas as pd
 import numpy as np
-import requests
 
-def sample_construct():
-    df = pd.read_pickle('../data/invoice_pickle')
-    practices = df.practice_id.unique()
-    practice_sub_sample = np.random.choice(practices, 10)
-    df_subsample = df[df.practice_id.isin(practice_sub_sample)]
-    df_subsample.to_pickle('../data/df_subsample')
+def sample_construct(sample=False):
+    df = pd.read_csv('../data/invoice_data.csv')
+    df.practice_enter_date = pd.to_datetime(df.practice_enter_date, infer_datetime_format=True)
+    df.transaction_date = pd.to_datetime(df.transaction_date, infer_datetime_format=True)
+
+    if sample:
+        practices = df.practice_id.unique()
+        practice_sub_sample = np.random.choice(practices, 10)
+        df_subsample = df[(df.practice_id.isin(practice_sub_sample)) & (df.transaction_type==1) & (df.practice_enter_date>'2015-11-01') & (df.transaction_date>'2015-11-01')]
+    else:
+        df_subsample = df[(df.transaction_type==1) & (df.practice_enter_date>'2015-11-01') & (df.transaction_date>'2015-11-01')] # keep only observations where transaction_type==1, keep if I have a year's worth of data, otherwise drop, keep observations only from the last year
+
+    FM_df = pd.DataFrame(df.practice_id.unique())
+
+    return FM_df, df_subsample
+
+
+    # df_subsample.to_pickle('../data/df_subsample')
+
+    # df_subsample = df[df.practice_id.isin(practice_sub_sample)]
+
 
 def patron_num():
-    df_invoice_ss = pd.read_pickle('../data/df_subsample')
+    # get list of unique users from the subsample dataset
+    # count these by practice
+    # append to feature matrix
 
-    df_client = pd.read_pickle('../data/clients_pickle')
-    d = pd.DataFrame(df_client.groupby('practice_id').size().rename('client_count'))
+    d = pd.DataFrame(df_subsample.groupby('practice_id').size().rename('client_count'))
     d['practice_id'] = d.index
 
     df_merged = pd.merge(df_invoice_ss, d, left_on=['practice_id'], right_on=['practice_id'], how='inner')
 
-    df_merged.to_pickle('../data/merged_ss')
 
-def patron_zip():
+
+    # df_invoice_ss = pd.read_pickle('../data/df_subsample')
+    #
+    # df_client = pd.read_pickle('../data/clients_pickle')
+    # d = pd.DataFrame(df_client.groupby('practice_id').size().rename('client_count'))
+    # d['practice_id'] = d.index
+    #
+    # df_merged = pd.merge(df_invoice_ss, d, left_on=['practice_id'], right_on=['practice_id'], how='inner')
+    #
+    # df_merged.to_pickle('../data/merged_ss')
+
+
+
+
+
+
+def zip_average_income():
     df = pd.read_csv('../data/14zpallagi.csv')
     df_sum = df.groupby('zipcode')[['N02650', 'A02650']].sum()
     df_sum['mean_income'] = df_sum['A02650']/df_sum['N02650']
@@ -33,9 +62,12 @@ def patron_zip():
 
     us_income = float(df_sum.loc['00000'].values)
     df_sum.drop(df_sum.index[0], inplace=True)
+    return us_income, df_sum
 
+def patron_zip():
     df = pd.read_pickle('../data/merged_ss')
-    # df.dropna(inplace=True)
+
+    us_income, df_sum = zip_average_income()
 
     practices = df.practice_id.unique()
     df['weighted_income'] = 0
@@ -46,7 +78,6 @@ def patron_zip():
 
         weighted_income = 0
         for zip_code in zips:
-            print(zip_code)
             try:
                 income = float(df_sum.loc[zip_code].values)
             except:
@@ -60,13 +91,29 @@ def patron_zip():
             weighted_income += (N_zip_code/N_practices) * income
 
         df.ix[df.practice_id == prac, 'weighted_income'] = weighted_income
-    print(df.head())
+    df.to_pickle('../data/merged_ss')
 
+# add this to feature matrix
+
+
+def practice_TR():
+    df = read_pickle('../data/merged_ss')
+    practices = df.practice_id.unique()
+
+    df['total_revenue'] = 0
+    for prac in practices:
+        float(practice_total_revenue(prac)[0])
+
+
+        df.ix[df.practice_id == prac, 'weighted_income'] = weighted_income
+
+# add this to feature matrix
 
 
 
 if __name__=="__main__":
     # sample_construct()
     # patron_num()
-    patron_zip()
-    # mean_income('60626')
+    # patron_zip()
+    # practice_TR()
+    print(float(practice_total_revenue('1')[0]))
